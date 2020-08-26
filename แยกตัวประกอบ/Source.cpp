b5b105e825db067ee3fb18da1eb53d802f4711aa
TonG <TonG@TONGTUNPHUK>
2020-08-26 23:41:06 +0700

    แก้ไขโค้ดครั้งที่ 1

--- แยกตัวประกอบ/Source.cpp
+++ แยกตัวประกอบ/Source.cpp
@@ -1,13 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <conio.h>
 
-
+//แก้ไขครั้งที่ 1    26/08/2563   เวลา  23:39 
 int main() {
     int k;
     int x = 2;
     printf("Input Number : ");
     scanf("%d", &k);
-    printf("Factoring Result :");
+    printf("Factoring Result : ");
     while (k == 1) {
         printf("%d\n", k);
         break;
@@ -15,17 +16,22 @@ int main() {
     while (k != 1) {
         while (k % x == 0) {
 
-           
-            printf("%d", x);
-            k = k / x;
-            while (k == 1) {
-                continue;
-            }
+         
+            printf("%d", x); 
             printf("x");
-
+            
+           
+            
+            k = k / x;
+             while (k == 1) {
+                printf("\b");
+                return 0;
+            }
+            
         }
 
         x++;
+        
     }
  
     return 0;
